a0e40970863772f5e35c2241103ba7ff1097b628
iota1996 <laudanum9527@gmail.com>
2017-11-26 22:36:16 +0800

    init a spring boot demo

---
+++ src/main/java/org/suanhua/LoanshopApplication.java
@@ -0,0 +1,12 @@
+package org.suanhua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanshopApplication.class, args);
+	}
+}
